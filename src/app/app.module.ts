f7948c45156ac628f09294de7eeb5be3df5ad960
Veronchi <zhigunova1995@gmail.com>
2022-02-06 14:04:20 +0300

    create note form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { AddBtnComponent } from './add-btn/add-btn.component';
 import { NotesComponent } from './notes/notes.component';
+import { NoteFormComponent } from './note-form/note-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     AddBtnComponent,
-    NotesComponent
+    NotesComponent,
+    NoteFormComponent
   ],
   imports: [
     BrowserModule,
